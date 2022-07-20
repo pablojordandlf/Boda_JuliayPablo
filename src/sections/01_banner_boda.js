c10cec3ca1bcc814f85befd1f22a77c2fba82d86
Pablo Jordán <pablo.jordan.dlf@gmail.com>
2022-07-20 12:58:41 +0200

    cambios

--- src/sections/01_banner_boda.js
+++ src/sections/01_banner_boda.js
@@ -1,7 +1,7 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui';
 import { Container, Box, Heading, Text, Image, Button, Flex } from 'theme-ui';
-import BannerImg from 'assets/salon_v1.png';
+import BannerImg from 'assets/STD3.png';
 
 
 export default function Banner_Boda() {
